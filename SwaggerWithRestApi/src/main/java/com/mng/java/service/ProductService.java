4cdc493d6ab334c6b07d904c190f9294e6c36d01
Mekala.Nagendra <Mekala.Nagendra@WKSBAN29RBW1060.corp.mphasis.com>
2018-11-12 19:22:21 +0530

    swagger docuumentaion with spring boot rest api

---
+++ SwaggerWithRestApi/src/main/java/com/mng/java/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.mng.java.service;
+
+import java.util.Map;
+
+import com.mng.java.model.Product;
+
+public interface ProductService {
+	Map<Integer,Product> listAllProducts();
+	Product getProductById(Integer id);
+	void saveProduct(Integer id,Product product);
+	void deleteProduct(Integer id);
+	boolean exists(Integer id);
+}
